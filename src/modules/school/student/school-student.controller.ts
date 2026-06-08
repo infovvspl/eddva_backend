c0bfc76c21e844fe3c6a98e9edd41e562d76da97
PRATAP KUMAR DAS <pratapkumar.das@rcm.ac>
2026-06-08 10:19:03 +0530

    Done

--- src/modules/school/student/school-student.controller.ts
+++ src/modules/school/student/school-student.controller.ts
@@ -14,7 +14,7 @@ export class SchoolStudentController {
   @Get() list(@SchoolUser() user: any, @Query() query: any) { return this.svc.list(user, query); }
   @Get('courses/my') myCourses(@SchoolUser() user: any) { return this.svc.getMyCourses(user); }
   @Get('courses/:classId') courseCurriculum(@SchoolUser() user: any, @Param('classId') classId: string) {
-    return this.svc.getCourseDetail(user, classId);
+    return this.svc.getCourseCurriculum(user, classId);
   }
   @Get(':id') findOne(@Param('id') id: string) { return this.svc.findOne(id); }
   @Put(':id') update(@Param('id') id: string, @Body() body: any) { return this.svc.update(id, body); }
